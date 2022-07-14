7727f5a3f615edf3a233cde00cd52090eb3ecb46
sdhtele <97067334+sdhtele@users.noreply.github.com>
2022-07-15 04:28:02 +0700

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -227,10 +227,10 @@ multi_rclone_init()
 # Pyrogram Client Intialization >>>>>>>>>>>
 app = Client("LeechBot", bot_token=TG_BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, workers=343)
 if STRING_SESSION:
-    userBot = Client("Tele-UserBot", api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-    LOGGER.info("[PRM] Initiated USERBOT") #Logging is Needed Very Much
+    
 
 updater = tg.Updater(token=TG_BOT_TOKEN)
 bot = updater.bot
 dispatcher = updater.dispatcher
 userBot = Client("Tele-UserBot", api_id=APP_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+LOGGER.info("[PRM] Initiated USERBOT") #Logging is Needed Very Much
